32743382951cdfddc6d9c1163f2310fbfa274cd6
BANYU AJI SENJANA <020002618@sonia.net>
2026-01-06 15:10:11 +0700

    updated

--- src/decorators/serialize.decorator.ts
+++ src/decorators/serialize.decorator.ts
@@ -1,6 +1,6 @@
 import { SetMetadata, applyDecorators } from '@nestjs/common';
 import { ClassConstructor } from 'class-transformer';
-import { RESPONSE_SERIALIZATION_META_KEY } from 'src/app/app.constant';
+import { RESPONSE_SERIALIZATION_META_KEY } from '../app/app.constant';
 
 export function Serialize(dto: ClassConstructor<any>) {
   return applyDecorators(SetMetadata(RESPONSE_SERIALIZATION_META_KEY, dto));
